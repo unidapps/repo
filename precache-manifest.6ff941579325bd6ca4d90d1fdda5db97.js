829b3d87fe6a8bbf9fc038333ccd3e299a88521d
unidapps <dappsuni@gmail.com>
2019-10-29 12:36:44 +0900

    Updates

---
+++ precache-manifest.6ff941579325bd6ca4d90d1fdda5db97.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a6c57ef995dc2fa31e9886eeb15692b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ee47a8c8bedd2a354f46",
+    "url": "/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "baa941cab782be88e59c",
+    "url": "/static/js/2.018da0b6.chunk.js"
+  },
+  {
+    "revision": "ee47a8c8bedd2a354f46",
+    "url": "/static/js/main.7fcad1d1.chunk.js"
+  },
+  {
+    "revision": "ffa42d7f2899d27e131e",
+    "url": "/static/js/runtime-main.da237e86.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/static/media/logo.25bf045c.svg"
+  }
+]);
